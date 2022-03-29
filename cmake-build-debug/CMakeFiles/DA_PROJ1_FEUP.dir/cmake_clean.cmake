06430a9e7e3445ece36af5d5a6dde1bb0491a76d
Ptozin <93348093+Ptozin@users.noreply.github.com>
2022-03-29 19:20:35 +0100

    new folder

---
+++ cmake-build-debug/CMakeFiles/DA_PROJ1_FEUP.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DA_PROJ1_FEUP.dir/src/menu.cpp.o"
+  "DA_PROJ1_FEUP"
+  "DA_PROJ1_FEUP.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DA_PROJ1_FEUP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/menu.cpp
@@ -0,0 +1,7 @@
+//
+// Created by arooz on 29/03/2022.
+//
+
+int main() {
+    return 0;
+}
